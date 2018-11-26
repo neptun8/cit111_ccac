552717676471bc2e501f7f375a1ca7636f8b2c7c
Sherzod Kulnazarov <sherzodkv@gmail.com>
2018-11-26 12:09:46 -0500

    Update PointOfSales.java

--- PointOfSales.java
+++ PointOfSales.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Simple POS Program.
+ * Pizza order POS, places order for pizza.
+ * User input dependent.
  */
 package pointofsales;
 
